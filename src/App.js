4bc56cb1e37a10f29beba6e22ed74e93d280fe9d
sherrysh13 <shaarifhasan13@gmail.com>
2024-03-19 18:11:36 +0500

    My ToDO App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [data, setdata] = useState('')
+  const [list, setlist] = useState([])
+
+  const clickHandler = () => {
+    console.log("btn clicked")
+    if(data === ""){
+      alert("please enter any input")
+    }else{
+      setlist([...list, data])
+      setdata("")
+      console.log(list)
+    }
+    
+  }
+  
+  const delHandler = (id) => {
+    const filteredArray = list.filter((elem, idx) => {
+      return(
+        idx !== id
+      )
+    })
+    setlist(filteredArray)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="inputField">
+          <input type="text" value={data} onChange={e => setdata(e.target.value)}/>
+          {/* {console.log(data)} */}
+          <button onClick={clickHandler}>Add Task</button>
+        </div>
+        <div className="listBox">
+          <div className="lists">
+            {
+              list.map((elem, idx) => {
+                return(
+                  <>
+                    <div className="list" key={idx}>
+                      <div className="txt"> {elem} </div>
+                      <button className='delBtn'onClick={() => delHandler(idx)}>DEL</button>
+                    </div>
+                  </>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
